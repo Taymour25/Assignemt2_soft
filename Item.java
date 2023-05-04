5b8d4d7390ec0570f816713b0830c984bba9051a
Tarek Mostafa <102294486+TarekMostafa20@users.noreply.github.com>
2023-05-04 13:40:58 +0300

    Update Item.java

--- Item.java
+++ Item.java
@@ -1,2 +1,18 @@
 public class Item {
+private int price;
+private String name;
+private int amountAvailable;
+private String soldBy;
+private int afterOfferPrice;
+private String category;
+private int soldToday;
+private int soldThisYear;
+
+public void addOffer();
+public float reCulatePrice();
+public void addToCart();
+public int getSoldToday();
+public int getSoldThisYear();
+public int getAmount();
+
 }
