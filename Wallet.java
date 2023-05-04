e39dc90c0f51c519b69adeb0f3a04a3396f9f1d0
Tarek Mostafa <102294486+TarekMostafa20@users.noreply.github.com>
2023-05-04 13:41:25 +0300

    Update Wallet.java

--- Wallet.java
+++ Wallet.java
@@ -1,2 +1,11 @@
 public class Wallet {
+private User user;
+private int balance;
+private String paymentType;
+
+public String changePaymentType();
+public int getBalance();
+public void rechargeWallet();
+public User getUser();
+
 }
