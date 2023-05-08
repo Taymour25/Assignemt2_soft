f7d510b2cded552624523e21e950b28fbf5f7acb
Tarek Mostafa <102294486+TarekMostafa20@users.noreply.github.com>
2023-05-08 18:05:34 +0300

    Update User.java

--- User.java
+++ User.java
@@ -1,3 +1,33 @@
+/*
+ * @author: Abdelrahman Taymour
+ * @modified: Tarek Mostafa Abo-Bakr
+ * @version: 0.1
+ * @date: 3/5
+ * 
+ * The User class extends the Account class and represents a user account in the system.
+ * It contains methods for editing, deleting the user account, sending a voucher, and retrieving the
+ * loyalty points of the user.
+ * 
+ * public class User extends Account{
+ * 
+ * The number of loyalty points the user has.
+ * private int loyaltyPoints;
+ * 
+ * Edits the user account information.
+ * public void editAccount() {}
+ * 
+ * Deletes the user account from the system.
+ * public void deleteAccount(){}
+ * 
+ * Sends a voucher to the user.
+ * public void sendVoucher(){}
+ * 
+ * Retrieves the number of loyalty points the user has.
+ * @return The number of loyalty points the user has.
+ * public void getLoyaltyPoints(){}
+ * }
+ */
+
 public class User extends Account
 {
     private int loyaltyPoints;
