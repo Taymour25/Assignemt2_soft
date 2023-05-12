249bea86288519721cf384e7d6c0bd7f5a987544
Tarek Mostafa <102294486+TarekMostafa20@users.noreply.github.com>
2023-05-12 20:06:08 +0300

    Update Visitor.java

--- Visitor.java
+++ Visitor.java
@@ -121,7 +121,7 @@ public class Visitor {
         return true;
     }
     private String table="Account";
-    public boolean login(String uname,String password) {
+    public int login(String uname,String password) {
         String sql = " SELECT * FROM " + table ;
 
         try {
@@ -133,46 +133,67 @@ public class Visitor {
              //loop through the result set
             while (result.next()) {
                 if(Objects.equals(result.getString("username"), uname) && Objects.equals(result.getString("pass"), password)){
-                    System.out.println("Welcome " + result.getString("username"));
+                    System.out.println("Welcome " + result.getString("username") );
+                    int v=result.getInt("ID");
                     conn.close();
-                   return true;
+                   return v;
                 }
             }
             conn.close();
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
-        return false;
+        return 0;
         }
 
-        public void register() throws SQLException {
+        public void register(String name,String pass) throws SQLException {
             System.out.println("Please Insert Your Data \n");
-            System.out.print("ID : ");
 
             Scanner scanner = new Scanner(System.in);
-            int theId= scanner.nextInt();
             Connect C=new Connect("tofee.db","Account");
             Connection conn = C.connect();
             Statement state = conn.createStatement();
-            if (check(theId)){
-                String Wquery="INSERT INTO Wallet VALUES (0," + theId + ",'Visa') ";
-                String Cquery="INSERT INTO Cart VALUES ("+ theId+",'',0,'cash') ";
-                System.out.print("Name : ");
-                String name= scanner.next();
-                System.out.print("password : ");
-                String pas= scanner.next();
                 System.out.print("Address : ");
                 String Addr= scanner.next();
                 System.out.print("Phone Number : ");
                 String phoneN= scanner.next();
-                C.insert(theId,name,pas,Addr,phoneN);
-                state.execute(Wquery);
-                state.execute(Cquery);
+                System.out.println("E-Mail :");
+                String mail =scanner.next();
+                C.insert(name,pass,Addr,phoneN,mail);
+                ResultSet R = state.executeQuery("Select * From Account WHERE username= '"+name+"'");
+            int theId=R.getInt("ID");
+            String Wquery="INSERT INTO Wallet VALUES (0," + theId + ",'Visa') ";
+            String Cquery="INSERT INTO Cart VALUES ("+ theId+",'',0,'cash') ";
+            state.execute(Wquery);
+            state.execute(Cquery);
                 conn.close();
-
-            }else {
-                System.out.println("ID already in use");
             }
+
+        public void forgetPassword() {
+            Scanner scanner = new Scanner(System.in);
+
+
+            System.out.println("Enter your username here : ");
+            String user = scanner.next();
+
+
+            OTPMAIL o = new OTPMAIL();
+            int otp = o.mn("tarekmostafa9444@gmail.com");
+            System.out.println("Your OTP: " + otp);
+            System.out.print("Enter the OTP: ");
+            int inputOtp = scanner.nextInt();
+
+            // Compare the input OTP with the generated OTP
+            if (inputOtp == otp) {
+                System.out.println("OTP confirmed successfully!");
+                System.out.println("access accepted");
+
+            } else {
+                System.out.println("Invalid OTP. Please try again.");
+                System.out.println("access denied");
             }
 
         }
+        }
+
+
