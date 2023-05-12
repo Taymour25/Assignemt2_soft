3de03108396f5a4db6bdc041b7a1ec5cdd963bfb
Tarek Mostafa <102294486+TarekMostafa20@users.noreply.github.com>
2023-05-12 20:04:45 +0300

    Update Connect.java

--- Connect.java
+++ Connect.java
@@ -15,7 +15,13 @@ public class Connect {
             database_name = DB_name;
             table_name = TB_name;
         }
-        public Connection connect() {
+
+    public static PreparedStatement prepareStatement(String s) {
+
+        return null;
+    }
+
+    public Connection connect() {
             Connection conn = null;
             try {
                 // db parameters
@@ -49,26 +55,47 @@ public class Connect {
             }
         }
 
-        public void insert(int ID, String name , String password,String Address,String Ph ) {
-            String sql = "INSERT INTO "+ table_name +"(ID, username,pass,address,phone,loyaltyPoints) VALUES(?,?,?,?,?,?)";
+        public void insert( String name , String password,String Address,String Ph,String mail ) {
+            String sql = "INSERT INTO "+ table_name +"( username,pass,address,phone,loyaltyPoints,mail) VALUES(?,?,?,?,?,?)";
 
             try{
                 Connection conn = this.connect();
                 PreparedStatement pstmt = conn.prepareStatement(sql);
-                pstmt.setInt(1, ID);
-                pstmt.setString(2, name);
-                pstmt.setString(3, password);
-                pstmt.setString(4, Address);
-                pstmt.setString(5, Ph);
-                pstmt.setInt(6, 0);
+                pstmt.setString(1, name);
+                pstmt.setString(2, password);
+                pstmt.setString(3, Address);
+                pstmt.setString(4, Ph);
+                pstmt.setInt(5, 0);
+                pstmt.setString(6,mail);
 
                 pstmt.executeUpdate();
                 conn.close();
             } catch (SQLException e) {
                 System.out.println(e.getMessage());
             }
-
         }
+
+
+    public void insertItem( int ID , int price,String name,String cat,String sold,int aval ) {
+        String sql = "INSERT INTO "+ table_name +"( itemID,price,name,category,SoldBy,quantityAvaiable,numberSold) VALUES(?,?,?,?,?,?,?)";
+
+        try{
+            Connection conn = this.connect();
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setInt(1, ID);
+            pstmt.setInt(2, price);
+            pstmt.setString(3, name);
+            pstmt.setString(4, cat);
+            pstmt.setString(5, sold);
+            pstmt.setInt(6,aval);
+            pstmt.setInt(7,0);
+
+            pstmt.executeUpdate();
+            conn.close();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
         public void selectAll() {
             String sql = " SELECT * FROM " + table_name;
 
@@ -78,9 +105,9 @@ public class Connect {
                 ResultSet rs = stmt.executeQuery(sql);
 
                 // loop through the result set
+                System.out.println("Item number Item Name             Item Price");
                 while (rs.next()) {
-                    System.out.println(rs.getInt("itemID") + "\t" +
-                            rs.getString("name") + "\t" + rs.getInt("price") + "$");
+                    System.out.println(rs.getInt("itemID")+"\t \t \t |"+rs.getString("name") + "\t |" + rs.getInt("price") + "$");
                 }
                 conn.close();
             } catch (SQLException e) {
@@ -88,5 +115,6 @@ public class Connect {
             }
 
         }
+
 
     }
