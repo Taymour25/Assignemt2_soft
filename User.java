dfe23c4f7fdb45b7c3680de44df4b96636332229
Tarek Mostafa <102294486+TarekMostafa20@users.noreply.github.com>
2023-05-12 20:05:48 +0300

    Update User.java

--- User.java
+++ User.java
@@ -31,7 +31,6 @@
 public class User extends Account
 {
     private int loyaltyPoints;
-    public void editAccount() {}
     public void deleteAccount(){}
     public void sendVoucher(){}
     public void getLoyaltyPoints(){}
